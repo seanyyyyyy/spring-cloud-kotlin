8e42b27227abbd9209e819a3e4b01fb32265ae29
sean.liaw <sean.liaw@afterpaytouch.com>
2020-04-03 14:30:10 +1100

    added junit properties, instance per class + tests

--- src/test/kotlin/com/example/cloudkotlin/hello/HelloRestTemplateTest.kt
+++ src/test/kotlin/com/example/cloudkotlin/hello/HelloRestTemplateTest.kt
@@ -2,6 +2,8 @@ package com.example.cloudkotlin.hello
 
 import com.example.cloudkotlin.CloudKotlinApplication
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
@@ -14,6 +16,11 @@ import org.springframework.http.HttpStatus
         webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class HelloRestTemplateTest(@Autowired var testRestTemplate: TestRestTemplate){
 
+    @BeforeAll
+    fun setup() {
+        println(">> Setup")
+    }
+
     @Test
     fun `Hello should return default message`() {
         val result = testRestTemplate
@@ -24,4 +31,13 @@ class HelloRestTemplateTest(@Autowired var testRestTemplate: TestRestTemplate){
         assertThat(result.body).contains("Hello, World")
     }
 
+    @Test
+    fun `Assert article page title, content and status code`() {
+        println(">> TODO")
+    }
+
+    @AfterAll
+    fun teardown() {
+        println(">> Tear down")
+    }
 }
